402273ab6adf0084e43504a7760f90a48f0a11b1
ahmedgamal2222 <159123998+ahmedgamal2222@users.noreply.github.com>
2024-08-29 01:24:06 +0300

    تعليق يصف التغييرات التي قمت بها

--- Final/admin.cs
+++
@@ -1,9 +0,0 @@
-﻿public class Admin
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Email { get; set; }
-    public string Password { get; set; }
-
-    //public virtual ICollection<User> Users { get; set; }
-}
