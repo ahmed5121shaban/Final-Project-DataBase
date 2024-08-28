f9e0c4a8917c447f0eefc4eb2ce7bd575c91b2fb
Ahmed Shaban <105598769+ahmed5121shaban@users.noreply.github.com>
2024-08-28 14:00:36 +0300

    First commit

---
+++ Final/FinalConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final
+{
+    internal class FinalConfiguration : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseSqlServer("");
+    }
+}
