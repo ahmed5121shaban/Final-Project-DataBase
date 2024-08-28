0bd8bff5b17df8484f49f06bf08bafe048693a7a
rowankhaled2902 <rowkhaled2902@gmail.com>
2024-08-28 15:22:40 +0300

    add chat and message class to main

---
+++ Final/Models/Chat.cs
@@ -0,0 +1,22 @@
+﻿using Final.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final
+{
+    public class Chat
+    {
+        public int Id { get; set; }
+        public int BuyerId { get; set; }
+        public int SellerId { get; set; }
+        public DateTime StartDate { get; set; }
+
+        public virtual User Buyer { get; set; }
+        public virtual User Seller { get; set; }
+        public virtual ICollection<Message> ChatMessages { get; set; }
+
+    }
+}
---
+++ Final/Models/Message.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public DateTime Time { get; set; }
+        public int ChatId { get; set; }
+
+        public virtual Chat Chat { get; set; }
+    }
+}
