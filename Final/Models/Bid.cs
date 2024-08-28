8fe7b88c280ca814afe6ce2d204f693b14429276
Sara Abdalla <159088471+SaraMohameddSayed@users.noreply.github.com>
2024-08-28 14:08:32 +0200

    Bid-Notification-Category

---
+++ Final/Models/Bid.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace finalproject.Models
+{
+    public class Bid
+    {
+        public  decimal Amount { get; set; }    
+
+        public DateTime Time { get; set; }
+
+        public int Auction_Id { get; set; }    
+
+        public int Buyer_Id { get; set; }
+
+    }
+}
