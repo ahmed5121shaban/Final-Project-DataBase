7aaed787561568c6bfd185a1936bf3a0a0a86da2
Sara Abdalla <159088471+SaraMohameddSayed@users.noreply.github.com>
2024-09-23 23:24:53 +0300

    update itemviewmodel and itemextensions

--- ModelView/Item/AddItemViewModel.cs
+++ ModelView/Item/AddItemViewModel.cs
@@ -8,5 +8,16 @@ namespace ModelView
 {
     public class AddItemViewModel
     {
+        public string Title { get; set; }
+        public int Category { get; set; }
+        public string Description { get; set; }
+
+        public int? sellPrice { get; set; }
+        public int startPrice { get; set; }
+        public string PaymentMethod { get; set; }
+
+        public IFormFile? Contract { get; set; }
+        public List<IFormFile> Images { get; set; }
+
     }
 }
--- ModelView/Item/ItemExtensions.cs
+++ ModelView/Item/ItemExtensions.cs
@@ -1,12 +1,63 @@
-﻿using System;
+﻿using Final;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace ModelView.Item
+using Microsoft.AspNetCore.Http;
+namespace ModelView
 {
-    public class ItemExtensions
+    public static class ItemExtensions
     {
+        public static Item toItemModel(this AddItemViewModel model)
+        {
+            List<Image> images = new List<Image>();
+            foreach (IFormFile file in model.Images)
+            {
+
+                string filename = DateTime.Now.ToFileTime().ToString() + file.FileName;
+                string path = Path.Combine(
+                     Directory.GetCurrentDirectory(),
+                "wwwroot",
+                "Images",
+                "Items",
+                filename
+                );
+                FileStream filestream = new FileStream(path, FileMode.Create);
+                file.CopyTo(filestream);
+                filestream.Close();
+                Image img = new Image { Src = Path.Combine("Images", "Items", filename) };
+                images.Add(img);
+            }
+
+            string fileName = DateTime.Now.ToFileTime().ToString() + model.Contract.FileName;
+            string filePath = Path.Combine(
+                Directory.GetCurrentDirectory(),
+                "wwwroot",
+                "Contracts",
+
+                fileName
+                );
+            FileStream stream = new FileStream(filePath, FileMode.Create);
+            model.Contract.CopyTo(stream);
+            stream.Close();
+            var contractfile = Path.Combine("Contracts", fileName);
+            return new Item
+            {
+                Name = model.Title,
+                Description = model.Description,
+                CategoryID = model.Category,
+                StartPrice = model.startPrice,
+                EndPrice = model.sellPrice ?? 0,
+                AddTime = DateTime.Now,
+                PublishDate = DateTime.Now,
+                IsReviewed = false,
+                AuctionID = 0,
+                Images = images,
+                ContractFile = contractfile,
+                UserID = "1",
+                ReviewID = 3
+            };
+        }
     }
 }
