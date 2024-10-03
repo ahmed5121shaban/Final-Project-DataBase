118a955cbb3df2aa34394da3203471080f4d0f04
Ahmed Shaban <105598769+ahmed5121shaban@users.noreply.github.com>
2024-10-03 09:48:44 +0300

    Update .gitignore file

--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6eb093e0f43064f1c8990e01112d567218079b4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc23c8c0f432aa3828106135d1746b32aa63ff18")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
