5fa6c9b4018017be56bdb0c102a896b78149880f
Sara Abdalla <159088471+SaraMohameddSayed@users.noreply.github.com>
2024-10-08 11:08:47 +0300

    auction controller added

---
+++ FinalApi/Controllers/AuctionController.cs
@@ -0,0 +1,42 @@
+﻿using Managers;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Microsoft.AspNetCore.Mvc;
+using ModelView;
+namespace FinalApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuctionController:ControllerBase
+    {
+        AuctionManager auctionManager;
+        public AuctionController(AuctionManager _auctionManager)
+        {
+            this.auctionManager= _auctionManager;   
+        }
+
+
+        [HttpGet("getall")]
+        public async Task<IActionResult> GetAll()
+        {
+            var auctions =  auctionManager.GetAll();
+            return Ok(auctions);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddAuction(AddAuctionModel _item)
+        {
+
+            var result =await auctionManager.Add(_item.toAuctionModel());
+            if (result == true)
+            {
+                return Ok();
+            }
+            else
+            {
+                return BadRequest(result);
+            }
+        }
+
+
+    }
+}
